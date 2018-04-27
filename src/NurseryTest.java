e1ccf6c2cd94ad62889505f7915529b7c8df1b82
Robin Åstedt <robin.astedt@gmail.com>
2018-04-27 12:31:17 +0200

    Added more example code

--- src/NurseryTest.java
+++ src/NurseryTest.java
@@ -9,14 +9,22 @@ public class NurseryTest {
         });
 
         Nursery.with((Nursery nursery) -> {
+            nursery.startSoon(() -> {
+                nursery.startSoon(() -> System.out.println("Nested scopes test 1"));
+                nursery.startSoon(() -> System.out.println("Nested scopes test 2"));
+            });
+            nursery.startSoon(() -> System.out.println("Nested scopes test 3"));
+        });
+
+        Nursery.with((Nursery nursery) -> {
             nursery.startSoon(NurseryTest::test1, "test 1");
             nursery.startSoon(NurseryTest::test2, "test 2");
         });
     }
 
     private static void test1() {
-        for (int i = 0; i < 100; i++) {
-            System.out.println("Test 1: " + i);
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Exception test 1: " + i);
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
@@ -25,11 +33,11 @@ public class NurseryTest {
         }
     }
     private static void test2() {
-        for (int i = 0; i < 100; i++) {
-            if (i == 50) {
+        for (int i = 0; i < 10; i++) {
+            if (i == 5) {
                 throw new NullPointerException();
             }
-            System.out.println("Test 2: " + i);
+            System.out.println("Exception test 2: " + i);
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
